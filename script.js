d4a41f5fb3c56354df7c42b0ac0aba2890febd61
Aadil Nuhu <aadilnuhu122@gmail.com>
2024-06-26 10:55:49 -0700

    commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// let currentSlide = 0;
+
+// const Slides = document.querySelectorAll('.slide');
+
+// function showSlide(index) {
+//     Slides.forEach(slide => slide.classList.remove('active'));
+
+//     Slides[index].classList.add('active');
+// }
+
+// function nextSlide() {
+//     currentSlide ++;
+//     if (currentSlide >= Slides.length) {
+//         currentSlide = 0;
+//     }
+//     showSlide(currentSlide);
+// }
+// setInterval(nextSlide,3000);
